eeaac6b2eb97ae8a10730a9e320c2fc718931c6f
cliffamadeus <reconcliffy@gmail.com>
2025-02-17 11:22:51 +0800

    Refactor

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -48,8 +48,6 @@ if ($stmt = $pdo->prepare($sql)) {
     unset($stmt);
 }
 
-
-
 // Fetch the most recent login per user
 $recentLogins = [];
 $sql = "
@@ -88,7 +86,6 @@ if ($stmt = $pdo->prepare($sql)) {
     }
     unset($stmt);
 }
-
 
 ?>
 
@@ -147,28 +144,6 @@ if ($stmt = $pdo->prepare($sql)) {
             </button>
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                    <!--
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="#">Link</a>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                            Dropdown
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li><a class="dropdown-item" href="#">Action</a></li>
-                            <li><a class="dropdown-item" href="#">Another action</a></li>
-                            <li><hr class="dropdown-divider"></li>
-                            <li><a class="dropdown-item" href="#">Something else here</a></li>
-                        </ul>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-                    </li>
-                    -->
                 </ul>
                 <form class="d-flex" role="search">
                     <a href="../logout.php" class="btn btn-danger">Logout</a>
